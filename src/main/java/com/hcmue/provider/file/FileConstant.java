71965433df9f2ecbe5f9eaa88aae48688fef2fdf
huytht <huytht2001@gmail.com>
2022-09-01 10:45:01 +0700

    update base folder image

--- src/main/java/com/hcmue/provider/file/FileConstant.java
+++ src/main/java/com/hcmue/provider/file/FileConstant.java
@@ -2,10 +2,12 @@ package com.hcmue.provider.file;
 
 public class FileConstant {
 	
-	// private static final String BASE_FOLDER = "E:/PetStoreImage";
-	private static final String BASE_FOLDER = System.getProperty("user.dir") + "/src/main/resources";
-	public static final String IMAGE_FOLDER = BASE_FOLDER + "/images/";
+//	private static final String BASE_FOLDER = System.getProperty("user.dir") + "/src/main/resources";
 
+	private static final String BASE_FOLDER = System.getProperty("user.dir");
+	
+	public static final String IMAGE_FOLDER = BASE_FOLDER + "/images/";
+	
 	public static final String TEMP_PROFILE_IMAGE_BASE_URL = "https://robohash.org/";
 
 	public static final String USER_URL_PATH = "/myfile/images/";
